4a020c73436c34cc664e53e37d34aedfe54bee7b
ajaberi <adnan.ajaberi@gmail.com>
2024-12-08 12:55:13 +0300

    got the login and signup page, connection to api not done

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
+import { LoginComponent } from './pages/login.component';
+import { SignupComponent } from './pages/signup.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    }
+];
