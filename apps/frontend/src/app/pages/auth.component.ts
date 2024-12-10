f4cc6ac840d1dd1ce2a5a6f270fde0b0852a4af2
ajaberi <adnan.ajaberi@gmail.com>
2024-12-10 10:49:10 +0300

    added auth page

---
+++ apps/frontend/src/app/pages/auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth',
+  imports: [CommonModule],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {}
