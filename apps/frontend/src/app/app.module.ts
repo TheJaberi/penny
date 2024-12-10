0baa156efbe8673596dc3e893399a6dc30992319
ajaberi <adnan.ajaberi@gmail.com>
2024-12-10 19:07:27 +0300

    fixes

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -5,14 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { LoginComponent } from './pages/login.component';
-import { SignupComponent } from './pages/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent
   ],
   imports: [
     BrowserModule,
